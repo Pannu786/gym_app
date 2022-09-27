6d37e4af9ebc20fc9f87b414908afa13ba308628
Pannu786 <thepannu786@gmail.com>
2022-09-27 22:51:10 +0530

    basic GET fetch dne with SearchExercises cmp

--- src/pages/ExcerciseDetail.js
+++ src/pages/ExcerciseDetail.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const ExcerciseDetail = () => {
   return <div>ExcerciseDetail</div>;
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import useState from 'react';
+import React from 'react';
+import { useState } from 'react';
 import { Box } from '@mui/material';
 
 import HeroBanner from '../components/HeroBanner';
