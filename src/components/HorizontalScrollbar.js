5291591eda36cbba5b27b9038a15a97814acc97b
Pannu786 <thepannu786@gmail.com>
2022-10-03 22:20:21 +0530

    BodyPart cmp dne

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, { useContext } from 'react';
+import { Box, Typography } from '@mui/material';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
 import BodyPart from './BodyPart';
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className='right-arrow'>
+      <img src={LeftArrowIcon} alt='right-arrow' />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className='left-arrow'>
+      <img src={RightArrowIcon} alt='right-arrow' />
+    </Typography>
+  );
+};
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <div>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((i) => (
         <Box key={i.id || i} itemId={i.id || i} title={i.id || i} m='0 40px'>
           <BodyPart item={i} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
       ))}
-    </div>
+    </ScrollMenu>
   );
 };
 
