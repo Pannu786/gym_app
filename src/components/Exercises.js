673fc1daaa0de6a5a8a38ce024a72b6b9361327c
Pannu786 <thepannu786@gmail.com>
2022-10-09 00:08:44 +0530

    found error on Exercises cpm

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -7,7 +7,27 @@ import ExercisesCard from './ExercisesCard';
 
 const Exercises = ({ bodyPart, setExercises, exercises }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const exercisesPerPage = 9;
+  const [exercisesPerPage] = useState(6);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData(
+          'https://exercisedb.p.rapidapi.com/exercises',
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+      setExercises(exercisesData);
+    };
+    fetchExercisesData();
+  }, [bodyPart]);
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
@@ -21,6 +41,7 @@ const Exercises = ({ bodyPart, setExercises, exercises }) => {
 
     window.scroll({ top: 1800, behavior: 'smooth' });
   };
+
   return (
     <Box
       id='exercises'
